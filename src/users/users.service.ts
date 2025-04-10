70a42099cd8c00da33688966a595c78744c04bb2
Felipe Escalona <escalonaf12@gmail.com>
2025-04-10 15:55:41 -0400

    Update user usign Object Assign

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,4 +14,27 @@ export class UsersService {
         return this.repo.save(user);
 
     }
+
+
+    findOne(id:number){
+      return this.repo.findOneBy({id});  
+
+    }
+
+    find(email:string){
+        return this.repo.find({where: {email}});
+    }
+
+
+    async update( id:number,attrs:Partial<User>){
+        const user = await this.findOne(id);
+        if(!user){
+            throw new Error('user not found');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+
+    }
+
+    remove(){}
 }
