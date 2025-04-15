df0f8ca476b3d8c20eb97fc401aad370e752b540
Felipe Escalona <escalonaf12@gmail.com>
2025-04-15 12:25:45 -0400

    Handle sign in and sign up

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { create } from 'domain';
+
+
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>) {
